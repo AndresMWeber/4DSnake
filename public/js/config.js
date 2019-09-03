6452e9b7af85322d9c6c88550ca6820906cf1541
Andres Weber <andresmweber@gmail.com>
2019-09-02 23:35:17 -0400

    Just figured out that the movement bug is gimbal lock (stops rotating and rotates around the Z axis at certain points).  Will look into this.

---
+++ public/js/config.js
@@ -0,0 +1,61 @@
+var AMOUNT = 6
+var ASPECT_RATIO = window.innerWidth / window.innerHeight
+var SCREEN_WIDTH = window.innerWidth * window.devicePixelRatio
+var SCREEN_HEIGHT = window.innerHeight * window.devicePixelRatio
+var loader,
+    renderer,
+    scene,
+    camera,
+    player,
+    board,
+    boardSize = 10,
+    food = []
+const KEYCODES = {
+    up: 38,
+    down: 40,
+    left: 37,
+    right: 39,
+    w: 87,
+    s: 83,
+    a: 65,
+    d: 68
+}
+
+var mat_flat_blue = new THREE.MeshToonMaterial({
+    ambient: 0x000000,
+    color: 0x48C4DA,
+    specular: 0x000000,
+    opacity: 0.05,
+    transparent: true,
+    shininess: 0,
+    shading: THREE.FlatShading
+})
+
+var mat_flat_orange = new THREE.MeshPhongMaterial({
+    ambient: 0x000000,
+    color: 0xFBB059,
+    specular: 0x000000,
+    shininess: 0,
+    shading: THREE.FlatShading
+})
+
+var mat_dark_orange = new THREE.MeshPhongMaterial({
+    ambient: 0x000000,
+    color: 0xCA4E2B,
+    specular: 0x000000,
+    shininess: 0,
+    shading: THREE.FlatShading
+})
+
+var mat_mid_blue = new THREE.MeshPhongMaterial({
+    ambient: 0x000000,
+    color: 0x365E81,
+    specular: 0x000000,
+    shininess: 0,
+    shading: THREE.FlatShading
+})
+
+var board_material = new THREE.LineBasicMaterial({
+    color: 0xFCEF9F,
+    linewidth: 2
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,22 @@
-var loader, renderer, scene, camera, player, board
-boardSize = 10
-
-var AMOUNT = 6;
-var ASPECT_RATIO = window.innerWidth / window.innerHeight;
-var WIDTH = (window.innerWidth / AMOUNT) * window.devicePixelRatio;
-var HEIGHT = (window.innerHeight / AMOUNT) * window.devicePixelRatio;
-const KEYCODES = {
-    up: 38,
-    down: 40,
-    left: 37,
-    right: 39,
-    w: 87,
-    s: 65,
-    a: 83,
-    d: 68
-}
-
-const ANGULAR_SPEED = 0.05
-const MOVEMENTS = {
-    ArrowUp: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), THREE.Math.degToRad(ANGULAR_SPEED)),
-    ArrowDown: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), THREE.Math.degToRad(-ANGULAR_SPEED * 6)),
-    ArrowLeft: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), THREE.Math.degToRad(-ANGULAR_SPEED * 6)),
-    ArrowRight: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), THREE.Math.degToRad(ANGULAR_SPEED * 6)),
-}
-
 document.onkeydown = function(e) {
     switch (e.keyCode) {
 
         case (KEYCODES.w):
-            player.rotateZ(90)
+            console.log('w')
+            player.up()
             break;
         case (KEYCODES.a):
-            player.rotateY(90)
+            console.log('a')
+            player.left()
             break;
         case (KEYCODES.s):
-            player.rotateY(270)
+            console.log('s')
+            player.down()
             break;
         case (KEYCODES.d):
-            player.rotateZ(270)
+            console.log('d')
+            player.right()
             break;
-    }
-    if (MOVEMENTS[e.key]) {
-        const cur = player.mesh.quaternion
-        const rot = MOVEMENTS[e.key]
-        cur.multiplyQuaternions(rot, cur)
     }
 }
 
@@ -53,33 +26,52 @@ class Snake {
         this.speed = .05
         this.moveTicker = 0
         this.direction = [0, 0, 1]
-        this.geometry = new THREE.BoxGeometry(1, 1, 1);
+        this.geometry = new THREE.BoxGeometry(.7, .7, .7);
         this.mesh = new THREE.Mesh(this.geometry, material);
+        // DIRECTION INDICATOR
+        let geometry = new THREE.BoxGeometry(.5, .5, 2);
+        let mesh = new THREE.Mesh(geometry, mat_mid_blue);
+        mesh.position.z = 1
+        this.mesh.add(mesh)
+        let scope = this
         this.moveQueue = []
+        loader.load('models/snake.fbx', function(object) {
+            object.traverse(child => { if (child.isMesh) child.material = mat_flat_blue })
+            this.add(object)
+        })
     }
 
-    rotateZ(degrees) {
-        this.moveQueue.push(() => player.mesh.rotateZ(THREE.Math.degToRad(degrees)))
-    }
-
-    rotateX(degrees) {
-        this.moveQueue.push(() => player.mesh.rotateX(THREE.Math.degToRad(degrees)))
-    }
-
-    rotateY(degrees) {
-        this.moveQueue.push(() => player.mesh.rotateY(THREE.Math.degToRad(degrees)))
-
-    }
-    checkRotationQueue() {
-        console.log(this.moveTicker)
-        if (!(this.moveTicker % 20) && this.moveQueue.length) {
-            console.log('MOVING')
-            this.moveQueue.shift()()
+    setOrientation(position, rotation) {
+        if (this.mesh) {
+            this.mesh.position.copy(position)
+            this.mesh.rotation.copy(rotation)
         }
     }
 
-    eat() {
+    up() {
+        this.moveQueue.push(() => this.mesh.rotation.x += 1.5708)
+    }
 
+    down() {
+        this.moveQueue.push(() => this.mesh.rotation.x -= 1.5708)
+    }
+
+    left() {
+        this.moveQueue.push(() => this.mesh.rotation.y -= 1.5708)
+    }
+
+    right() {
+        this.moveQueue.push(() => this.mesh.rotation.y += 1.5708)
+
+    }
+
+    checkRotationQueue() {
+        if (!(Object.values(this.mesh.position).every(pos => pos % 1 < .04 && pos % 1 > 0.96) && this.moveQueue.length)) {
+            console.log('QUEUE ACTIVATED')
+            let func = this.moveQueue.shift()
+            if (func) func()
+            this.mesh.updateMatrixWorld()
+        }
     }
 
     update() {
@@ -89,13 +81,15 @@ class Snake {
         this.mesh.getWorldDirection(wpVector)
 
         this.direction = wpVector.toArray()
-
         this.direction.map((dirNormal, i) => {
             if (dirNormal) {
                 let futurePosition = this.mesh.position[this.dirs[i]] + this.speed * dirNormal
-                if (futurePosition <= 0 - boardSize / 2 + 1 || futurePosition >= boardSize / 2 - 1) {
-                    // console.log('flegh.')
-                } else if (dirNormal) {
+                if (futurePosition <= 0 - boardSize / 2 || futurePosition >= boardSize / 2 - 1) {
+                    futurePosition = this.mesh.position[this.dirs[i]] + -.05 * dirNormal
+                    console.log('Reversing direction, out of bounds!', this.speed)
+                }
+
+                if (dirNormal) {
                     this.mesh.position[this.dirs[i]] = futurePosition
                     this.moveTicker += 1
                 }
@@ -108,22 +102,18 @@ function initScene() {
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     loader = new THREE.FBXLoader();
     scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2(0xf0fff0, 0.05);
+    // scene.fog = new THREE.FogExp2(0xf0fff0, 0.05);
     maincamera = new THREE.PerspectiveCamera(50, ASPECT_RATIO, 0.1, 1000);
-    subcamera = new THREE.PerspectiveCamera(40, ASPECT_RATIO, 0.1, 10);
-    subcamera.viewport = new THREE.Vector4(0, 0, Math.ceil(WIDTH), Math.ceil(HEIGHT));
+    subcamera = new THREE.PerspectiveCamera(40, ASPECT_RATIO, 0.1, 100);
 
-    var cameras = []
-    cameras.push(maincamera)
-    cameras.push(subcamera)
-    camera = new THREE.ArrayCamera(cameras);
     stats = new Stats();
 
     maincamera.position.x = 10
     maincamera.position.y = 7
     maincamera.position.z = 7
-    subcamera.position.y = 10
-    subcamera.position.z = -5
+    subcamera.position.y = 1
+    subcamera.position.z = -1
+    subcamera.rotation.y -= 1.5708 * 2
     renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.autoClear = false;
@@ -139,43 +129,11 @@ function initScene() {
 }
 
 function createObjects() {
-    var mat_flat_blue = new THREE.MeshToonMaterial({
-        ambient: 0x000000,
-        color: 0x48C4DA,
-        specular: 0x000000,
-        opacity: 0.05,
-        transparent: true,
-        shininess: 0,
-        shading: THREE.FlatShading
-    })
-
-    var mat_flat_orange = new THREE.MeshPhongMaterial({
-        ambient: 0x000000,
-        color: 0xFBB059,
-        specular: 0x000000,
-        shininess: 0,
-        shading: THREE.FlatShading
-    })
-
-    var mat_wireframe = new THREE.MeshPhongMaterial({
-        color: 0xffffff,
-        wireframe: true,
-        side: THREE.DoubleSide
-    });
-
 
     player = new Snake(mat_flat_orange)
+    player.mesh.add(subcamera)
 
-    var geo = new THREE.EdgesGeometry(new THREE.BoxGeometry(10, 10, 10)); // or WireframeGeometry( geometry )
-
-    var mat = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 2 });
-    var wireframe = new THREE.LineSegments(geo, mat);
-    wireframe.position.x -= .5
-    wireframe.position.y -= .5
-    wireframe.position.z -= .5
-    scene.add(wireframe);
-
-    loader.load('models/tile.fbx', function(object) {
+    loader.load('models/dot.fbx', function(object) {
         object.traverse(child => { if (child.isMesh) child.material = mat_flat_blue });
         for (let i = 0; i < boardSize; i++) {
             for (let j = 0; j < boardSize; j++) {
@@ -190,12 +148,50 @@ function createObjects() {
         }
     })
 
-    // var lineSegments = new THREE.LineSegments(board, new THREE.LineDashedMaterial({ color: 0xffaa00, dashSize: 3, gapSize: 1 }))
-    // lineSegments.computeLineDistances()
+    var board = new THREE.EdgesGeometry(new THREE.BoxGeometry(10, 10, 10)); // or WireframeGeometry( geometry )
 
-    // scene.add(lineSegments)
+    var lineSegments = new THREE.LineSegments(board, new THREE.LineDashedMaterial({
+        color: 0xffaa00,
+        dashSize: 3,
+        gapSize: 1
+    }))
+    lineSegments.position.x -= .5
+    lineSegments.position.y -= .5
+    lineSegments.position.z -= .5
+    lineSegments.computeLineDistances()
+
+    // scene.add(wireframe);
+    scene.add(lineSegments)
     scene.add(player.mesh)
-        // scene.add(board)
+}
+
+function generateRandomPosition() {
+    return [Math.floor(Math.random() * boardSize), Math.floor(Math.random() * boardSize), Math.floor(Math.random() * boardSize)]
+}
+
+function spawnFood(numFood) {
+    let foodPositions = []
+    for (let i = 0; i < numFood; i++) {
+        let posCandidate = generateRandomPosition()
+        while (foodPositions.includes(posCandidate)) {
+            posCandidate = generateRandomPosition()
+        }
+        foodPositions.push(posCandidate)
+    }
+    foodPositions.map(foodPosition => {
+        var mesh = new THREE.Mesh(this.geometry, mat_mid_blue);
+
+        loader.load('models/apple.fbx', function(object) {
+            object.traverse(child => { if (child.isMesh) child.material = mat_dark_orange })
+            mesh.add(object)
+        })
+
+        mesh.position.x = foodPosition[0] - boardSize / 2
+        mesh.position.y = foodPosition[1] - boardSize / 2
+        mesh.position.z = foodPosition[2] - boardSize / 2
+        food.push(mesh)
+        scene.add(mesh)
+    })
 }
 
 function createLights() {
@@ -211,28 +207,30 @@ function createLights() {
 }
 
 function updateScene() {
-    // cube.rotation.x += .01;
-    // board.rotation.x += .01;
     player.update()
 }
 
 function animate() {
     requestAnimationFrame(animate);
     updateScene()
+    stats.update();
     renderer.clear();
+    renderer.setViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     renderer.render(scene, maincamera);
+
+    renderer.setViewport(0, 0, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4);
     renderer.render(scene, subcamera);
 }
 
 window.addEventListener('resize', () => {
-    var ASPECT_RATIO = window.innerWidth / window.innerHeight;
-    var WIDTH = (window.innerWidth / AMOUNT) * window.devicePixelRatio;
-    var HEIGHT = (window.innerHeight / AMOUNT) * window.devicePixelRatio;
+    ASPECT_RATIO = window.innerWidth / window.innerHeight;
+    SCREEN_WIDTH = window.innerWidth * window.devicePixelRatio;
+    SCREEN_HEIGHT = window.innerHeight * window.devicePixelRatio;
 
     maincamera.aspect = ASPECT_RATIO;
     maincamera.updateProjectionMatrix();
-    camera.aspect = ASPECT_RATIO;
-    camera.updateProjectionMatrix();
+    subcamera.aspect = ASPECT_RATIO;
+    subcamera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }, false)
 
@@ -240,8 +238,8 @@ function main() {
     initScene()
     createObjects()
     createLights()
-        // animate()
-    setInterval(animate(), 1000)
+    spawnFood(5)
+    animate()
 }
 
-main();
+main()
