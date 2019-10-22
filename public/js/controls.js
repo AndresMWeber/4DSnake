7d257b5fc6d3595c03adffefc8a9cc83baef037c
Andres Weber <andresmweber@gmail.com>
2019-10-22 02:10:08 -0400

    Finished controls.

--- public/js/controls.js
+++ public/js/controls.js
@@ -5,110 +5,54 @@ var lat,
 
 document.onkeydown = function(e) {
     switch (e.keyCode) {
-        case (KEYCODES.w):
+        case (KEY_CODES.w):
             player.addMove(player.pitchUp.bind(player))
             break;
-        case (KEYCODES.a):
+        case (KEY_CODES.a):
             player.addMove(player.left.bind(player))
             break;
-        case (KEYCODES.s):
+        case (KEY_CODES.s):
             player.addMove(player.pitchDown.bind(player))
             break;
-        case (KEYCODES.d):
+        case (KEY_CODES.d):
             player.addMove(player.right.bind(player))
             break;
-        case (KEYCODES.left):
+        case (KEY_CODES.left):
             camera.rotation.y -= .1
             camera.updateProjectionMatrix()
             break;
-        case (KEYCODES.right):
+        case (KEY_CODES.right):
             camera.rotation.y += .1
             camera.updateProjectionMatrix()
             break;
-        case (KEYCODES.up):
+        case (KEY_CODES.up):
             camera.zoom += .05
             camera.updateProjectionMatrix()
             break;
-        case (KEYCODES.down):
+        case (KEY_CODES.down):
             camera.zoom -= .05
             camera.updateProjectionMatrix()
             break;
     }
 }
 
-tjs_controls = new THREE.OrbitControls(tjs_camera, tjs_renderer.domElement)
-tjs_controls.target.set(0, 0, 0)
-tjs_controls.enableDamping = true
-tjs_controls.dampingFactor = 1
-tjs_controls.enableZoom = true
-tjs_controls.enablePan = false
-tjs_controls.touches = {
-    TWO: THREE.TOUCH.ROTATE,
-    THREE: THREE.TOUCH.DOLLY_PAN
-}
 
-// tjs_renderer.domElement.addEventListener('touchstart', onDocumentTouchStart, { passive: true })
-// tjs_renderer.domElement.addEventListener('touchmove', onDocumentTouchMove, false);
-// tjs_renderer.domElement.addEventListener('touchend', onDocumentTouchEnd, false);
-// document.createElement('button')
-
-tjs_renderer.domElement.addEventListener("touchstart", startTouch, false);
-tjs_renderer.domElement.addEventListener("touchmove", moveTouch, false);
-
-var initialX = null;
-var initialY = null;
-
-function startTouch(e) {
-    initialX = e.touches[0].clientX;
-    initialY = e.touches[0].clientY;
-};
-
-document.onload(
+window.onload = () => {
     isMobileDevice() && tjs_scene.remove(player.compass.group)
-)
+
+    tjs_controls = new THREE.OrbitControls(tjs_camera, tjs_renderer.domElement)
+    tjs_controls.target.set(0, 0, 0)
+    tjs_controls.enableDamping = true
+    tjs_controls.dampingFactor = 1
+    tjs_controls.enableZoom = true
+    tjs_controls.enablePan = false
+    tjs_controls.touches = {
+        ONE: null,
+        TWO: THREE.TOUCH.ROTATE,
+        THREE: THREE.TOUCH.PAN
+    }
+}
 
 function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
-
-function moveTouch(e) {
-    if (initialX === null || initialY === null) return
-
-    var currentX = e.touches[0].clientX;
-    var currentY = e.touches[0].clientY;
-
-    var diffX = initialX - currentX;
-    var diffY = initialY - currentY;
-
-    if (Math.abs(diffX) > Math.abs(diffY)) {
-        if (diffX > 0) {
-            player.addMove(player.left.bind(player))
-        } else {
-            player.addMove(player.right.bind(player))
-        }
-    } else {
-        if (diffY > 0) {
-            player.addMove(player.pitchUp.bind(player))
-        } else {
-            player.addMove(player.pitchDown.bind(player))
-        }
-    }
-
-    initialX = null;
-    initialY = null;
-
-    e.preventDefault();
-};
-
-
-function onDocumentTouchMove(event) {
-    if (event.touches.length == 2) {
-        event.preventDefault();
-        touch_info = event.touches
-        tjs_camera.rotation.x += 2
-    }
-}
-
-function onDocumentTouchEnd(event) {
-    touch_info = []
-}
